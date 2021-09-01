4ed42940e935eff3ef5a285e56bb9e8116ff7b8b
Nikita Baturin <nikitabaturin@rambler.ru>
2021-09-01 18:55:05 +0300

    Add nodejs server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.sendDate = false;
+    res.statusCode = 599;
+
+    res.setHeader('Author', 'Nikita Baturin');
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello client');
+}) 
+
+server.listen(9999, () => console.log('Server listening on 9999 port'));
