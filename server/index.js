2b4d3239332dd35bd2a135acdcf3bc7bd6259e68
Nikita Baturin <nikitabaturin@rambler.ru>
2021-09-08 20:08:16 +0300

    Deploy app

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const http = require('http');
+const hasParams = require('../utils/hasParams');
+
+const serverOrigin = 'http://localhost:3000/'
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+    response.setHeader('Content-Type', 'text/html')
 
-console.log("Server running at http://localhost:%d", port);
+    switch(true) {
+        case request.url === '/': {
+            response.end('<strong>Hello client</strong><ul><li><a href="/user">User page</a></li></ul>');
+            break;
+        }
+        case hasParams(request.url, '/user'): {
+            const currentUrl = new URL(request.url, serverOrigin);
+            const name = currentUrl.searchParams.get('name');
+
+            response.end(`Hello <strong>${name}</strong>`);
+            break;
+        }
+        case request.url === '/user': {
+            response.end('Hello Nikita<ul><li><a href="/">Home page</a></li></ul>');
+            break;
+        }
+        default: response.end('Hello client!');
+    }
+}) 
+
+module.exports = server;
